ca55f7936854e3f976736b84762c711e6a89cf79
EC2 Default User <ec2-user@ip-172-31-43-100.ec2.internal>
2021-06-04 09:09:21 +0000

    更新:Ruby_8章まで

---
+++ add2.rb
@@ -0,0 +1,7 @@
+for i in 1..6 do
+  puts i
+end
+
+for s in 1..10 do
+  puts s
+end
---
+++ amounts2.rb
@@ -0,0 +1,9 @@
+age = {"さき"=>28, "みずき"=>30, "ゆうき"=>34}
+age.each do |name, age|
+   puts "#{name}は#{age}歳です。"
+end
+
+amounts = {"リンゴ"=>2, "イチゴ"=>5, "オレンジ"=>3}
+amounts.each do |fruit, amount|
+  puts "#{fruit}は#{amount}個です。"
+end
